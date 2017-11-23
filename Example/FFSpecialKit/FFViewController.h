7077232569999fc92aa1fdc00202dd482d2d8517
lingdf <lfidc2008@gmail.com>
2017-11-23 14:21:04 +0800

    Initial commit

---
+++ Example/FFSpecialKit/FFViewController.h
@@ -0,0 +1,13 @@
+//
+//  FFViewController.h
+//  FFSpecialKit
+//
+//  Created by lingdf on 11/23/2017.
+//  Copyright (c) 2017 lingdf. All rights reserved.
+//
+
+@import UIKit;
+
+@interface FFViewController : UIViewController
+
+@end
---
+++ Example/FFSpecialKit/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  FFSpecialKit
+//
+//  Created by lingdf on 11/23/2017.
+//  Copyright (c) 2017 lingdf. All rights reserved.
+//
+
+@import UIKit;
+#import "FFAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FFAppDelegate class]));
+    }
+}
